030adac430054a2f91170f932887abcd48e99b72
Patrick Coston <patcoston@gmail.com>
2022-10-03 16:48:19 -0400

    Fixed Router based on changes in v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
+//import { Route, Routes, BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'
 
-function App() {
+import Home from './Home'
+import Software from './Software'
+import Games from './Games'
+import Misc from './Misc'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">Header</header>
-    </div>
+    <BrowserRouter>
+      Test
+      <div>
+        <nav>
+          Navigation
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/software">Software</Link>
+            </li>
+            <li>
+              <Link to="/games">Games</Link>
+            </li>
+            <li>
+              <Link to="/misc">Misc</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/software" element={<Software />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/misc" element={<Misc />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
